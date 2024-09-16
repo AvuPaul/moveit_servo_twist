a217ef93d0e2902a30f096b6613674051e3e335e
AvuPaul <P.Janssen@avular.com>
2024-09-16 16:22:09 +0000

    updated parameters

--- build/moveit_servo_kinova/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/humble/src/gtest_vendor
-# Build directory: /ros2_ws/src/moveit_servo_kinova/build/moveit_servo_kinova/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
